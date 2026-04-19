b60c97f7b8bb4a34cd4db513145b4a1003c76a3c
Tutul <tutulkabir@gmail.com>
2026-04-19 13:00:56 +0530

    part-1 completed

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,16 @@
+import PublicFooter from "@/components/shared/PublicFooter";
+import PublicNavbar from "@/components/shared/PublicNavbar";
+import React from "react";
+
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <PublicNavbar />
+      {children}
+
+      <PublicFooter />
+    </>
+  );
+};
+
+export default CommonLayout;
